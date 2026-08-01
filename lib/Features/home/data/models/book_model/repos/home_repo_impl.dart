b99b5ccb3c6bc4060d15b5eb17a4f28555a0999a
Osama <samirosama127@gmail.com>
2026-08-01 15:44:26 +0300

    create api service

---
+++ lib/Features/home/data/models/book_model/repos/home_repo_impl.dart
@@ -0,0 +1,19 @@
+import 'package:bookly/Core/errors/failure.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/models/book_model/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> featchFeaturedBooks() {
+    // TODO: implement featchFeaturedBooks
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> featchBestSellerBooks() {
+    // TODO: implement featchBestSellerBooks
+    throw UnimplementedError();
+  }
+}
