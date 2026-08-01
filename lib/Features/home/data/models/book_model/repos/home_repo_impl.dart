65777b5b46dfcf0b5a5d7b2629a63f84583bdf1c
Osama <samirosama127@gmail.com>
2026-08-01 18:19:04 +0300

    implement featch newest books

--- lib/Features/home/data/models/book_model/repos/home_repo_impl.dart
+++ lib/Features/home/data/models/book_model/repos/home_repo_impl.dart
@@ -1,19 +1,36 @@
 import 'package:bookly/Core/errors/failure.dart';
+import 'package:bookly/Core/utils/api_service.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/data/models/book_model/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
-  
+  ApiService apiService;
+  HomeRepoImpl(this.apiService);
+
   @override
   Future<Either<Failure, List<BookModel>>> featchFeaturedBooks() {
     // TODO: implement featchFeaturedBooks
     throw UnimplementedError();
   }
-  
+
   @override
-  Future<Either<Failure, List<BookModel>>> featchBestSellerBooks() {
-    // TODO: implement featchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> featchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            'volumes?filter=free-ebooks&q=sports&key=AIzaSyBp6fM4i8456OeDg4Uai8piFwafT-3Zv5M&Sorting=newest',
+      );
+
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 }
