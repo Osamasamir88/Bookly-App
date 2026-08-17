4d30ab3d86fff142e9abda017e39b491df3aaa93
Osama <samirosama127@gmail.com>
2026-08-17 19:59:30 +0300

    setup service locator

---
+++ lib/Core/utils/service_locator.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Core/utils/api_service.dart';
+import 'package:bookly/Features/home/data/models/book_model/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupServiceLocator() {
+  // 1. أوبجكت Dio موحد للمشروع كله
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  // 2. أوبجكت HomeRepoImpl موحد للمشروع كله
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiService>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:bookly/Core/routing/router_generation_config.dart';
 import 'package:bookly/Core/styles/app_colors.dart';
+import 'package:bookly/Core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
