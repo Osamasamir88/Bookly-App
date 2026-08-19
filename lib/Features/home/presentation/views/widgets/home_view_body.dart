d15437472fa712f1123834e0485ebddfd5e36447
Osama <samirosama127@gmail.com>
2026-08-19 16:05:58 +0300

    add Refresh Indicator to home screen

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,39 +1,48 @@
 import 'package:bookly/Core/styles/app_text_styles.dart';
+import 'package:bookly/Features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/book_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CustomAppBar(),
-                BooksListview(),
-                Text('Best Seller', style: AppTextStyles.titleMed),
-                SizedBox(height: 20),
-              ],
+    return RefreshIndicator(
+      onRefresh: () async {
+        // تحديث كل البيانات في الشاشة دفعة واحدة
+        await BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+        // لما نخلص ال newest نبقي نضيف الميثود بتعتها
+      },
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CustomAppBar(),
+                  BooksListview(),
+                  Text('Best Seller', style: AppTextStyles.titleMed),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
           ),
-        ),
 
-        // best seller list view
-        SliverList.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => BookListViewItem(),
-        ),
+          // best seller list view
+          SliverList.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) => BookListViewItem(),
+          ),
 
-        SliverToBoxAdapter(child: SizedBox(height: 30)),
-      ],
+          SliverToBoxAdapter(child: SizedBox(height: 30)),
+        ],
+      ),
     );
   }
 }
