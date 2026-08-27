ba72e781bdb94902f8629b24560896f481bd9b82
Osama <samirosama127@gmail.com>
2026-08-27 19:06:33 +0300

    create similar books cubit

---
+++ lib/Features/home/presentation/view_models/similar_books_cubit/similar_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/models/book_model/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'similar_books_state.dart';
+
+class SimilarBooksCubit extends Cubit<SimilarBooksState> {
+  SimilarBooksCubit(this.homeRepo) : super(SimilarBooksInitial());
+  final HomeRepo homeRepo;
+
+  featchSimilarBooks(String category) async {
+    emit(SimilarBooksLoading());
+    var result = await homeRepo.featchSimilarBooks(category);
+    result.fold(
+      (failure) => emit(SimilarBooksFailure(failure.errMassage)),
+      (books) => emit(SimilarBooksSuccess(books)),
+    );
+  }
+}
